aa143c8ecb3a412668925b2c7e25e915175db639
skcary77 <skcary@gmail.com>
2025-07-17 12:51:54 -0400

    updated facets

--- api/shared_code/__init__.py
+++ api/shared_code/__init__.py
@@ -4,7 +4,8 @@ import os
 def azure_config():
 
     configs = {}
-    configs["search_facets"] = os.environ.get("SearchFacets", "")
+    # configs["search_facets"] = os.environ.get("SearchFacets", "")
+    configs["search_facets"] = "primary_channels"
     configs["search_index_name"] = os.environ.get("SearchIndexName", "")
     configs["search_service_name"] = os.environ.get("SearchServiceName", "")
     configs["search_api_key"] = os.environ.get("SearchApiKey", "")
